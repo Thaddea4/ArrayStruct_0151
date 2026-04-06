04799f3c2ce9bb332d46a0a03487e0b178a62a8a
Thaddea4 <adeyasyie@gmail.com>
2026-04-06 16:07:13 +0700

    memperbaiki yang error

--- array.cpp
+++ array.cpp
@@ -1,34 +1,57 @@
 #include <iostream>
 using namespace std;
 
+int main() {
+    // Deklarasi array dengan inisialisasi
+    int aku[5] = {10, 20, 30, 40, 50};
 
-int main() {  
-    // deklarasi dengan inisialisasi
-    int aku [5] = {10,20,30,40,50};  
-    // deklarasi array tanpa inisialisasi
+    // Deklarasi array tanpa inisialisasi (hanya cadangan)
     int kamu[5];
-    // tampilkan data
+
+    // Tampilkan data awal array aku
     cout << "Data pada index 1 = " << aku[1] << endl;
-    // ganti index ke 1
+
+    // Ganti nilai index ke 1
     aku[1] = 200;
-    cout << endl;
+
+    // Tampilkan data setelah diubah
     cout << "Data pada index 1 = " << aku[1] << endl;
-    // mengisi satu per satu
+
+    // Mengisi Satu per Satu dengan input dari user
     cout << "Data index 0 = ";
     cin >> kamu[0];
+    
     cout << "Data index 1 = ";
     cin >> kamu[1];
+    
     cout << "Data index 2 = ";
     cin >> kamu[2];
+    
     cout << "Data index 3 = ";
     cin >> kamu[3];
+    
     cout << "Data index 4 = ";
     cin >> kamu[4];
     cout << endl;
-    // menampilkan satu per satu
-    cout << "Data Pertama = " << kamu[0] << endl;
+
+    // Mengisi data dengan menggunakan perulangan for
+    cout << "\n=== Data yang telah diinput ===" << endl;
+    cout << "Data pertama = " << kamu[0] << endl;
     cout << "Data kedua = " << kamu[1] << endl;
-    cout << "Data Ketiga = " << kamu[2] << endl;
-    cout << "Data Keempat = " << kamu[3] << endl;
+    cout << "Data ketiga = " << kamu[2] << endl;
+    cout << "Data keempat = " << kamu[3] << endl;
     cout << "Data kelima = " << kamu[4] << endl;
     cout << endl;
+
+    //mengisi dengan perulangan for
+    for (int i = 0; i < 4; i++) {
+        cout << "Data index ke-" << i << " = ";
+        cin >> kamu[i];
+    }
+    cout << endl;
+
+    //menampilkan dengan menggunakan perulangan for
+    for (int i = 0; i < 4; i++) {
+        cout << "Data ke-" << i << " = " << kamu[i] << endl;
+    }
+}
